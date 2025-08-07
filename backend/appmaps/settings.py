be2c5770a6f0dfc58252b1b85dd198b471b7b874
cristiangodoyangel <creacciones@gmail.com>
2025-08-07 00:04:07 -0400

    fix: conexión db corregida

--- backend/appmaps/settings.py
+++ backend/appmaps/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret! dfggdfdgdfgsdf
 SECRET_KEY = 'django-insecure-x*c)0f7c1_uvcmm-z8)!4*28u87+7c5#ya9277fjrl&6iu(bqy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -73,13 +73,12 @@ WSGI_APPLICATION = 'appmaps.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'misrutas',
         'USER': 'postgres',
-        'PASSWORD': 'NuevaPassword123',
+        'PASSWORD': 'calama1.',
         'HOST': 'localhost',  # ✅ CAMBIA esto
         'PORT': '5432',
         'OPTIONS': {
@@ -87,11 +86,6 @@ DATABASES = {
         }
     }
 }
-
-
-
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
